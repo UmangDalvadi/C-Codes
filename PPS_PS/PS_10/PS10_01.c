3be28b5a5582b20b4024337b358070fcc98ce451
UmangDalvadi <umangdalvadi8@gmail.com>
2022-07-20 01:07:38 +0530

    add

--- PPS_PS/PS_10/PS10_01.c
+++ PPS_PS/PS_10/PS10_01.c
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -13,20 +13,4 @@ int main()
     fclose(ptr);
 
     return 0;
-=======
-#include <stdio.h>
-#include <stdlib.h>
-
-int main()
-{
-    FILE *ptr = NULL;
-    ptr = fopen("file of pr01.txt", "r");
-    char c[33];
-    fgets(c, 33, ptr);
-    printf("contents of given file is : %s", c);
-
-    fclose(ptr);
-
-    return 0;
->>>>>>> f18087919f4bde72de56ea398bf50a37e3115842
 }
