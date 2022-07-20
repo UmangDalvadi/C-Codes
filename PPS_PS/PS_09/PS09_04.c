197fd53a8778c61d4ee6c26beee7545f9e1b4413
UmangDalvadi <umangdalvadi8@gmail.com>
2022-07-20 20:01:38 +0530

    add

--- PPS_PS/PS_09/PS09_04.c
+++ PPS_PS/PS_09/PS09_04.c
@@ -2,16 +2,16 @@
 
 int main()
 {
-    int *ptr, bhargav[6], sum = 0;
+    int *ptr, arr[6], sum = 0;
 
     printf("Enter the number : \n");
 
     for (int i = 0; i < 6; i++)
     {
-        scanf("%d", &bhargav[i]);
+        scanf("%d", &arr[i]);
     }
 
-    ptr = &bhargav[0];
+    ptr = &arr[0];
 
     for (int i = 0; i < 6; i++)
     {
