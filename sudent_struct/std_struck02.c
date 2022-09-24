51060e0c6aad026612fa8ed72cda39e912a2841d
UmangDalvadi <umangdalvadi8@gmail.com>
2022-09-25 05:21:02 +0530

    add final student structure

--- sudent_struct/std_struck02.c
+++ sudent_struct/std_struck02.c
@@ -27,13 +27,167 @@ void display(struct student s[])
     int i = 0;
     while (fread(&s[i], sizeof(struct student), 1, fp))
     {
-        if (i < 5)
+        printf("Name : %s\nDepartment : %s\nEnrollment : %d\nSemster : %d\nSPI : %.2f\n\n", s[i].name, s[i].dept, s[i].enroll, s[i].sem, s[i].spi);
+        i++;
+    }
+    fclose(fp);
+}
+
+void search(struct student s[])
+{
+    printf("====================================================\n");
+
+    FILE *fp;
+    fp = fopen("std_data.txt", "r");
+    int enroll;
+    printf("Enter Enrollment number to search: ");
+    scanf("%d", &enroll);
+    printf("\n");
+    int i = 0;
+    while (fread(&s[i], sizeof(struct student), 1, fp))
+    {
+        if (s[i].enroll == enroll)
         {
             printf("Name : %s\nDepartment : %s\nEnrollment : %d\nSemster : %d\nSPI : %.2f\n\n", s[i].name, s[i].dept, s[i].enroll, s[i].sem, s[i].spi);
             i++;
         }
+        else
+        {
+            printf("details not found!!!\n");
+        }
     }
     fclose(fp);
+}
+
+void sort_s(struct student s[], struct student temp, int n)
+{
+    printf("====================================================\n");
+
+    FILE *fp;
+    fp = fopen("std_data.txt", "r");
+    fseek(fp, 0, SEEK_END);
+    int i = ftell(fp) / sizeof(struct student);
+    rewind(fp);
+    for (int i = 0; i < n; i++)
+    {
+        fread(&s[i], sizeof(struct student), 1, fp);
+    }
+    fclose(fp);
+    // printf("Name : %s\nDepartment : %s\nEnrollment : %d\nSemster : %d\nSPI : %.2f\n\n", s[i].name, s[i].dept, s[i].enroll, s[i].sem, s[i].spi);
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = i + 1; j < n; j++)
+        {
+            if (s[j].spi > s[i].spi)
+            {
+                // float temp = s[j].spi;
+                // s[j].spi = s[i].spi;
+                // s[i].spi = temp;
+                temp = s[j];
+                s[j] = s[i];
+                s[i] = temp;
+            }
+        }
+    }
+    // fclose(fp);
+    fp = fopen("std_data.txt", "w");
+    for (int i = 0; i < n; i++)
+    {
+        printf("Name : %s\nDepartment : %s\nEnrollment : %d\nSemster : %d\nSPI : %.2f\n\n", s[i].name, s[i].dept, s[i].enroll, s[i].sem, s[i].spi);
+        fwrite(&s[i], sizeof(struct student), 1, fp);
+    }
+    fclose(fp);
+}
+
+void edit(struct student s[])
+{
+    printf("====================================================\n");
+
+    FILE *fp, *fp1;
+    fp = fopen("std_data.txt", "r");
+    fp1 = fopen("temp.txt", "w");
+
+    int enroll;
+    printf("Enter Enrollment number to edit: ");
+    scanf("%d", &enroll);
+    printf("\n");
+    int i = 0;
+    while (fread(&s[i], sizeof(struct student), 1, fp))
+    {
+        if (s[i].enroll == enroll)
+        {
+            // printf("Name : %s\nDepartment : %s\nEnrollment : %d\nSemster : %d\nSPI : %.2f\n\n", s[i].name, s[i].dept, s[i].enroll, s[i].sem, s[i].spi);
+            printf("\nEnter new name : ");
+            scanf("%s", &s[i].name);
+            printf("Enter new department : ");
+            scanf("%s", &s[i].dept);
+            printf("Enter new Enrollment : ");
+            scanf("%d", &s[i].enroll);
+            printf("Enter new semester : ");
+            scanf("%d", &s[i].sem);
+            printf("Enter new SPI : ");
+            scanf("%f", &s[i].spi);
+        }
+        else
+        {
+            printf("details not found!!!\n");
+        }
+        fwrite(&s[i], sizeof(struct student), 1, fp1);
+        i++;
+    }
+    fclose(fp);
+    fclose(fp1);
+
+    i = 0;
+    fp1 = fopen("temp.txt", "r");
+    fp = fopen("std_data.txt", "w");
+    while (fread(&s[i], sizeof(struct student), 1, fp1))
+    {
+        fwrite(&s[i], sizeof(struct student), 1, fp);
+        i++;
+    }
+    fclose(fp);
+    fclose(fp1);
+}
+
+void delete (struct student s[])
+{
+    printf("====================================================\n");
+
+    FILE *fp, *fp1;
+    fp = fopen("std_data.txt", "r");
+    fp1 = fopen("temp.txt", "w");
+
+    int enroll;
+    printf("Enter Enrollment number to delete: ");
+    scanf("%d", &enroll);
+    printf("\n");
+    int i = 0;
+    while (fread(&s[i], sizeof(struct student), 1, fp))
+    {
+        if (s[i].enroll == enroll)
+        {
+            // printf("Name : %s\nDepartment : %s\nEnrollment : %d\nSemster : %d\nSPI : %.2f\n\n", s[i].name, s[i].dept, s[i].enroll, s[i].sem, s[i].spi);
+        }
+        else
+        {
+            fwrite(&s[i], sizeof(struct student), 1, fp1);
+        }
+        i++;
+    }
+    fclose(fp);
+    fclose(fp1);
+
+    i = 0;
+    fp1 = fopen("temp.txt", "r");
+    fp = fopen("std_data.txt", "w");
+    while (fread(&s[i], sizeof(struct student), 1, fp1))
+    {
+        fwrite(&s[i], sizeof(struct student), 1, fp);
+        i++;
+    }
+    fclose(fp);
+    fclose(fp1);
 }
 
 int main()
@@ -42,7 +196,7 @@ int main()
     printf("\nEnter the number of students : ");
     scanf("%d", &n);
     printf("\n");
-    struct student s[n];
+    struct student s[n], temp;
     FILE *fp = NULL;
     fp = fopen("std_data.txt", "w");
     for (int i = 0; i < n; i++)
@@ -85,50 +239,58 @@ int main()
 
             break;
         case 2:
-            printf("====================================================\n");
-            int enrollment;
-            printf("Enter enrollment number : ");
-            scanf("%d", &enrollment);
-            for (int i = 0; i < n; i++)
-            {
-                if (s[i].enroll == enrollment)
-                {
-                    printf("\nName : %s\n", s[i].name);
-                    printf("Department : %s\n", s[i].dept);
-                    printf("Enrollment : %d\n", s[i].enroll);
-                    printf("Semster : %d\n", s[i].sem);
-                    printf("SPI : %.2f\n\n", s[i].spi);
-                }
-            }
+
+            // int enrollment;
+            // printf("Enter enrollment number : ");
+            // scanf("%d", &enrollment);
+            // for (int i = 0; i < n; i++)
+            // {
+            //     if (s[i].enroll == enrollment)
+            //     {
+            //         printf("\nName : %s\n", s[i].name);
+            //         printf("Department : %s\n", s[i].dept);
+            //         printf("Enrollment : %d\n", s[i].enroll);
+            //         printf("Semster : %d\n", s[i].sem);
+            //         printf("SPI : %.2f\n\n", s[i].spi);
+            //     }
+            // }
+            search(s);
             break;
         case 3:
-            printf("====================================================\n");
-            for (int i = 0; i < n - 1; i++)
-            {
-                for (int j = i + 1; j < n; j++)
-                {
-                    if (s[j].spi > s[i].spi)
-                    {
-                        int temp = s[j].spi;
-                        s[j].spi = s[i].spi;
-                        s[i].spi = temp;
-                    }
-                }
-            }
-            for (int i = 0; i < 5; i++)
-            {
-                printf("Name : %s ,SPI : %.3f\n", s[i].name, s[i].spi);
-            }
+            // printf("====================================================\n");
+            // for (int i = 0; i < n - 1; i++)
+            // {
+            //     for (int j = i + 1; j < n; j++)
+            //     {
+            //         if (s[j].spi > s[i].spi)
+            //         {
+            //             int temp = s[j].spi;
+            //             s[j].spi = s[i].spi;
+            //             s[i].spi = temp;
+            //         }
+            //     }
+            // }
+            // for (int i = 0; i < 5; i++)
+            // {
+            //     printf("Name : %s ,SPI : %.3f\n", s[i].name, s[i].spi);
+            // }
+            sort_s(s, temp, n);
 
             break;
         case 4:
-            // edit();
+            edit(s);
+            break;
+        case 5:
+            delete (s);
+            break;
 
         default:
+            // printf("Invalid Case!!!\n");
             break;
         }
 
     } while (n_exit != 6);
+    printf("Thank you");
 
     return 0;
 }
